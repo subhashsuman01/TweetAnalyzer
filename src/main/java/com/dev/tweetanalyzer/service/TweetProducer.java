7241c58d9f48b475362652c487c630c642d83171
subhashsuman01 <subhash.suman@sprinklr.com>
2022-08-16 15:56:30 +0530

    added okhttp to handle connection
    fixed a bug where receiving an EOF in empty buffer would terminate connection instead of waiting for data.

--- src/main/java/com/dev/tweetanalyzer/service/TweetProducer.java
+++ src/main/java/com/dev/tweetanalyzer/service/TweetProducer.java
@@ -2,6 +2,10 @@ package com.dev.tweetanalyzer.service;
 
 import com.dev.tweetanalyzer.data.TweetDataWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
@@ -9,11 +13,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Objects;
 
 @Component
@@ -29,27 +29,34 @@ public class TweetProducer implements CommandLineRunner {
 
     @Async
     private void start() throws IOException {
-        URL url = new URL(Objects.requireNonNull(env.getProperty("twitter.url")));
-        HttpURLConnection twitterConnection = (HttpURLConnection) url.openConnection();
-        twitterConnection.setRequestMethod("GET");
-        twitterConnection.setRequestProperty("Authorization", "Bearer " + env.getProperty("twitter.access.token"));
 
-        try (BufferedReader tweetStream = new BufferedReader(new InputStreamReader(twitterConnection.getInputStream()))) {
-            String inputLine;
-            while ((inputLine = tweetStream.readLine()) != null) {
-                if (Thread.interrupted()) {
-                    kafkaTemplate.flush();
-                    break;
+        OkHttpClient client = new OkHttpClient();
+
+
+        Request request = new Request.Builder()
+                .url(Objects.requireNonNull(env.getProperty("twitter.url")))
+                .addHeader("Authorization", "Bearer " + env.getProperty("twitter.access.token"))
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            while(!response.body().source().exhausted()){
+                String inputLine;
+                while ((inputLine = response.body().source().readUtf8Line()) != null){
+                    try {
+                        TweetDataWrapper tweetDataWrapper = tweetMapper.readValue(inputLine, TweetDataWrapper.class);
+
+                        if (tweetDataWrapper.data().referenced_tweets() != null)
+                            kafkaTemplate.send("reply", tweetDataWrapper.data().id(), inputLine);
+                        else
+                            kafkaTemplate.send("tweet", tweetDataWrapper.data().id(), inputLine);
+                    } catch (MismatchedInputException e){
+
+                    } catch (IOException e){
+                        throw new IOException(e);
+                    }
                 }
-                TweetDataWrapper tweetDataWrapper = tweetMapper.readValue(inputLine, TweetDataWrapper.class);
-                if (tweetDataWrapper.data().referenced_tweets() != null)
-                    kafkaTemplate.send("reply", tweetDataWrapper.data().id(), inputLine);
-                else
-                    kafkaTemplate.send("tweet", tweetDataWrapper.data().id(), inputLine);
             }
-        }
-        finally {
-            twitterConnection.disconnect();
         }
     }
 
